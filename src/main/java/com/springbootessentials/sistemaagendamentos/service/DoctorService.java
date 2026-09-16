e3430860bec5c98e3b3887f36fc488eec9465259
Pedro Milhomens <pmilhomens1211@gmail.com>
2026-09-15 21:59:47 -0400

    Adicao das annotations da Classe Doctor e variantes

--- src/main/java/com/springbootessentials/sistemaagendamentos/service/DoctorService.java
+++ src/main/java/com/springbootessentials/sistemaagendamentos/service/DoctorService.java
@@ -1,4 +1,12 @@
 package com.springbootessentials.sistemaagendamentos.service;
 
+import com.springbootessentials.sistemaagendamentos.repository.DoctorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class DoctorService {
+    private final DoctorRepository doctorRepository;
+
 }
